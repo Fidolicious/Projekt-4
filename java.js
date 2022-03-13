50e104b432b098169a49470851aad38a586fcfd9
Jeeanett <jeanett5@hotmail.com>
2022-03-13 21:22:16 +0100

    opdate

--- java.js
+++ java.js
@@ -37,7 +37,6 @@ document.addEventListener("DOMContentLoaded", () => {
   loginForm.addEventListener("submit", e => {
       e.preventDefault();
 
-      // Perform your AJAX/Fetch login
 
       setFormMessage(loginForm, "error", "Forkert brugernavn eller adgangskode");
   });
@@ -54,6 +53,11 @@ document.addEventListener("DOMContentLoaded", () => {
       });
   });
 });
+
+// ........ TIL TOPPEN ....... //
+
+
+
 // ANNE-SOFIE //
 // Cart
 let heartIcon = document.querySelector("#heart-icon")
@@ -219,34 +223,6 @@ window.onclick = function(event) {
 // ABDULLAHI //
 
 
-var i = 0; 			// Start Point
-var images = [];	// Images Array
-var time = 7000;	// Time Between Switch
-
-// Image List
-images[0] = "img/billede1.jpg";
-images[1] = "img/billede2.jpg";
-images[2] = "img/billede3.jpg";
-
-// Change Image
-function changeImg(){
-document.slide.src = images[i];
-
-// Check If Index Is Under Max
-if(i < images.length - 1){
-  // Add 1 to Index
-  i++;
-} else {
-  // Reset Back To O
-  i = 0;
-}
-
-// Run function every x seconds
-setTimeout("changeImg()", time);
-}
-
-// Run function when page loads
-window.onload=changeImg;
 
 
 
