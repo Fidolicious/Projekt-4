72bd853408f772efe8103da9d7a34a04f708779e
Anne-Sofie <95890841+Fidolicious@users.noreply.github.com>
2022-03-15 11:23:11 +0100

    top

--- bogoversigt.js
+++ bogoversigt.js
@@ -168,3 +168,23 @@ cartShopBox.getElementsByClassName("cart-quantity")[0].addEventListener("change"
 			document.getElementsByClassName("total-price")[0].innerText = "$" + total;
 
 	}
+
+	//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
